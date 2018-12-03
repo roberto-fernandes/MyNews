b3a057b4efe5afa72744a6814cb6270483dc5d71
Roberto Fernandes <roberto.fernandes21@gmail.com>
2018-12-03 15:48:14 +0000

    Tabs implementation

--- app/src/main/java/robfernandes/xyz/mynews/MainActivity.java
+++ app/src/main/java/robfernandes/xyz/mynews/MainActivity.java
@@ -1,6 +1,8 @@
 package robfernandes.xyz.mynews;
 
 import android.os.Bundle;
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
@@ -9,5 +11,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ViewPager viewPager = findViewById(R.id.activity_main_container);
+        setupViewPager(viewPager);
+        TabLayout tabLayout = findViewById(R.id.activity_main_tabs);
+        tabLayout.setupWithViewPager(viewPager);
     }
+
+    private void setupViewPager(ViewPager viewPager) {
+        PageAdapter adapter = new PageAdapter(getSupportFragmentManager());
+        adapter.addFragment(new MainFragment(), "TAB1");
+        adapter.addFragment(new MainFragment() , "TABsdf2");
+        adapter.addFragment(new MainFragment(), "TAB3");
+        viewPager.setAdapter(adapter);
+    }
+
 }
