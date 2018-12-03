a092df2417bab24e732ed9b581332f4d29c3b2bf
Roberto Fernandes <roberto.fernandes21@gmail.com>
2018-12-03 17:19:03 +0000

    Toolbar search button and vertical menu

--- app/src/main/java/robfernandes/xyz/mynews/MainActivity.java
+++ app/src/main/java/robfernandes/xyz/mynews/MainActivity.java
@@ -4,6 +4,8 @@ import android.os.Bundle;
 import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.support.v7.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,6 +18,21 @@ public class MainActivity extends AppCompatActivity {
         setupViewPager(viewPager);
         TabLayout tabLayout = findViewById(R.id.activity_main_tabs);
         tabLayout.setupWithViewPager(viewPager);
+        this.configureToolbar();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        //2 Inflate the menu and add it to the Toolbar
+        getMenuInflater().inflate(R.menu.menu_toolbar, menu);
+        return true;
+    }
+
+    private void configureToolbar(){
+        // Get the toolbar view inside the activity layout
+        Toolbar toolbar = findViewById(R.id.activity_main_toolbar);
+        // Sets the Toolbar
+        setSupportActionBar(toolbar);
     }
 
     private void setupViewPager(ViewPager viewPager) {
