23bae576578a149c6c7e6b49ef3cb502adac4e8e
Roberto Fernandes <roberto.fernandes21@gmail.com>
2018-12-07 20:50:29 +0000

    Preferences

---
+++ app/src/main/java/robfernandes/xyz/mynews/Controller/PreferenceActivity.java
@@ -0,0 +1,13 @@
+package robfernandes.xyz.mynews.Controller;
+
+import android.os.Bundle;
+
+/**
+ * Created by Roberto Fernandes on 07/12/2018.
+ */
+public class PreferenceActivity extends android.preference.PreferenceActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
