942ced6ef5813320ad53b6a0c340965cf1041b98
Roberto Fernandes <roberto.fernandes21@gmail.com>
2018-12-12 21:43:04 +0000

    Top Stories API call corrected

--- app/src/main/java/robfernandes/xyz/mynews/Controller/Fragments/TopStoriesFragment.java
+++ app/src/main/java/robfernandes/xyz/mynews/Controller/Fragments/TopStoriesFragment.java
@@ -7,16 +7,24 @@ import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import java.util.List;
 
-import robfernandes.xyz.mynews.Model.APIMethods;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+import robfernandes.xyz.mynews.Model.APICall;
 import robfernandes.xyz.mynews.Model.APIResponse;
 import robfernandes.xyz.mynews.R;
 import robfernandes.xyz.mynews.View.TopStoriesAdapter;
+
+import static robfernandes.xyz.mynews.Utils.Constants.TOP_STORIES_API_BASE_URL;
 
 
 /**
@@ -24,10 +32,11 @@ import robfernandes.xyz.mynews.View.TopStoriesAdapter;
  */
 public class TopStoriesFragment extends Fragment {
 
+    private static final String TAG = "TopStoriesFragment";
     private RecyclerView mRecyclerView;
     private TopStoriesAdapter mTopStoriesAdapter;
     private List<APIResponse.Result> mNewsList;
-    private APIMethods mAPIMethods;
+    private View view;
 
     public TopStoriesFragment() {
         // Required empty public constructor
@@ -37,19 +46,49 @@ public class TopStoriesFragment extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        View view = inflater.inflate(R.layout.fragment_top_stories, container, false);
-        mRecyclerView = view.findViewById(R.id.fragment_top_stories_recycler_view);
-        mRecyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
-        mRecyclerView.setAdapter(mTopStoriesAdapter);
+        view = inflater.inflate(R.layout.fragment_top_stories, container, false);
+        setTopStories();
         return view;
     }
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+    }
 
-        mAPIMethods = new APIMethods();
-        mNewsList = mAPIMethods.getTopStories();
+    private void setTopStories() {
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl(TOP_STORIES_API_BASE_URL)
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+
+        APICall apiCall = retrofit.create(APICall.class);
+
+        Call<APIResponse> call = apiCall.getNews();
+
+        call.enqueue(new Callback<APIResponse>() {
+            @Override
+            public void onResponse(Call<APIResponse> call, Response<APIResponse> response) {
+                if (response.isSuccessful()) {
+                    APIResponse apiResponse = response.body();
+                    mNewsList = apiResponse.getResults();
+                    displayNews();
+                } else {
+                    Log.e(TAG, "asd onResponse: not successful.." + response);
+                }
+            }
+
+            @Override
+            public void onFailure(Call<APIResponse> call, Throwable t) {
+                Log.e(TAG, "asd onFailure: on Top APIResponse API Call.. " + t);
+            }
+        });
+    }
+
+    private void displayNews() {
         mTopStoriesAdapter = new TopStoriesAdapter(mNewsList);
+        mRecyclerView = view.findViewById(R.id.fragment_top_stories_recycler_view);
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+        mRecyclerView.setAdapter(mTopStoriesAdapter);
     }
 }
