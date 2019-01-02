a70e98336ba4632cda4bf654379f098a2d739f5f
Rohan Taneja <r.taneja@fvfx237hhv2h.ad.deliveryhero.com>
2019-01-02 17:54:50 +0100

    added hidden api_key in circle ci script

---
+++ app/src/main/java/robfernandes/xyz/mynews/utils/APIKeys.java
@@ -0,0 +1,8 @@
+package robfernandes.xyz.mynews.utils;
+
+import robfernandes.xyz.mynews.BuildConfig;
+
+public class APIKeys {
+
+    public static final String API_KEY = BuildConfig.MY_API_KEY;
+}
