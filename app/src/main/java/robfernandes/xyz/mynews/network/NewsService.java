388fbea8e1d98855fa422d3258016c780838d8e9
rob9233 <roberto.fernandes21@gmail.com>
2019-01-02 14:03:19 +0000

    NewsService Static import

--- app/src/main/java/robfernandes/xyz/mynews/network/NewsService.java
+++ app/src/main/java/robfernandes/xyz/mynews/network/NewsService.java
@@ -4,10 +4,11 @@ import retrofit2.Call;
 import retrofit2.http.GET;
 import retrofit2.http.Path;
 import retrofit2.http.Query;
-import robfernandes.xyz.mynews.utils.APIKeys;
 import robfernandes.xyz.mynews.network.model.APIResponseMostPopular;
 import robfernandes.xyz.mynews.network.model.APIResponseSearch;
 import robfernandes.xyz.mynews.network.model.APIResponseTopStories;
+
+import static robfernandes.xyz.mynews.utils.APIKeys.API_KEY;
 
 /**
  * Created by Roberto Fernandes on 12/12/2018.
@@ -15,15 +16,15 @@ import robfernandes.xyz.mynews.network.model.APIResponseTopStories;
 @SuppressWarnings("SpellCheckingInspection")
 public interface NewsService {
 
-    @GET("topstories/v2/{section}.json?api_key=" + APIKeys.API_KEY)
+    @GET("topstories/v2/{section}.json?api_key=" + API_KEY)
     Call<APIResponseTopStories> TopStories(@Path("section") String section);
 
-    @GET("mostpopular/v2/mostviewed/all-sections/7.json?api_key=" + APIKeys.API_KEY)
+    @GET("mostpopular/v2/mostviewed/all-sections/7.json?api_key=" + API_KEY)
     Call<APIResponseMostPopular> mostPopular();
 
     @GET("search/v2/articlesearch.json?&fl=section_name," +
             "web_url,headline,multimedia,snippet,pub_dat&api_key=" +
-            APIKeys.API_KEY)
+            API_KEY)
     Call<APIResponseSearch> search(@Query("q") String term, @Query("begin_date") String beginDate,
                                    @Query("end_date") String endDate);
 }
