7a2b13afc0518706e4dbf656d045749423332fd4
rob9233 <roberto.fernandes21@gmail.com>
2019-01-07 14:21:08 +0000

    Circle ci config var name changed

---
+++ app/src/main/java/robfernandes/xyz/mynews/utils/APIKeys.java
@@ -0,0 +1,10 @@
+package robfernandes.xyz.mynews.utils;
+
+import robfernandes.xyz.mynews.BuildConfig;
+
+/**
+ * Created by Roberto Fernandes on 07/01/2019.
+ */
+public class APIKeys {
+    public static final String API_KEY = BuildConfig.MY_API_KEY;
+}
